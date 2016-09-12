9a49a9675fb0df92337bee05648a63ae9542f0c0
jhallida <jhallida@indiana.edu>
2016-09-12 16:35:19 -0400

    third fix to match new solr naming scheme

--- lib/tasks/batch_import.rb
+++ lib/tasks/batch_import.rb
@@ -64,7 +64,7 @@ module Cbrc
               obj.depositor = owner.email
               obj.edit_users = [owner.email]
               obj.rights = ['http://creativecommons.org/licenses/by-nc/3.0/us/']
-              obj.collectionCode = ['herbarium']
+              obj.collection_code = ['herbarium']
               obj.identifier = ["http://purl.dlib.indiana.edu/iudl/herbarium/#{cat_num}"]
               obj.set_read_groups( ["public"], [])
             end
